d479ae0c71a72ead368600ff83bca5675d9f18e8
Sahil Kumar <sahilkumar04522@gmail.com>
2026-04-08 13:41:22 +0530

    fix: correct django settings module path

--- musicapp/musicapp/wsgi.py
+++ musicapp/musicapp/wsgi.py
@@ -11,6 +11,5 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musicapp.settings')
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'musicapp.musicapp.settings')
 application = get_wsgi_application()
